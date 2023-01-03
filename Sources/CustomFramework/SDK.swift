d4613d4a1a92ce132ef5528ee8a048396d527a2d
ayemereodia2 <odiadavid2@gmail.com>
2023-01-04 00:33:14 +0100

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by ANDELA on 04/01/2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
